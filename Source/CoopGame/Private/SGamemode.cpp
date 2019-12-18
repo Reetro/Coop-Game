74c94eddb6549c933a41a433d19636aa82ca507c
Reetro <thegamerboy7@gmail.com>
2019-12-17 17:38:11 -0700

    Started to setup wave logic

--- Source/CoopGame/Private/SGamemode.cpp
+++ Source/CoopGame/Private/SGamemode.cpp
@@ -2,4 +2,52 @@
 
 
 #include "SGamemode.h"
+#include "TimerManager.h"
 
+
+ASGamemode::ASGamemode()
+{
+  TimeBetweenWaves = 2.0f;
+}
+
+void ASGamemode::StartWave()
+{
+  WaveCount++;
+
+  NumOfBotsToSpawn = 2 * WaveCount;
+
+  GetWorldTimerManager().SetTimer(TimerHandel_BotSpawner, this, &ASGamemode::SpawnBotTimerElasped, 1.0f, true, 0.0f);
+}
+
+void ASGamemode::SpawnBotTimerElasped()
+{
+  SpawnNewBot();
+
+  NumOfBotsToSpawn--;
+
+  if (NumOfBotsToSpawn <= 0)
+  {
+    EndWave();
+  }
+}
+
+void ASGamemode::StartPlay()
+{
+  Super::StartPlay();
+
+  PrepareForNextWave();
+}
+
+void ASGamemode::EndWave()
+{
+  GetWorldTimerManager().ClearTimer(TimerHandel_BotSpawner);
+
+  PrepareForNextWave();
+}
+
+void ASGamemode::PrepareForNextWave()
+{
+  FTimerHandle TimerHandel_NextWaveStart;
+
+  GetWorldTimerManager().SetTimer(TimerHandel_NextWaveStart, this, &ASGamemode::StartWave, TimeBetweenWaves, false);
+}
--- Source/CoopGame/Public/SGamemode.h
+++ Source/CoopGame/Public/SGamemode.h
@@ -14,4 +14,35 @@ class COOPGAME_API ASGamemode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+  // Hook for BP to spawn a single bot
+  UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+  void SpawnNewBot();
+
+  // Start bot spawning
+  void StartWave();
+
+  // Stop spawning bots
+  void EndWave();
+
+  // Timer for next wave
+  void PrepareForNextWave();
+
+  void SpawnBotTimerElasped();
+
+  FTimerHandle TimerHandel_BotSpawner;
+
+  // Number of bots to spawn in current wave
+  int32 NumOfBotsToSpawn;
+
+  int32 WaveCount;
+
+  UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+  float TimeBetweenWaves;
+
+public:
+
+  ASGamemode();
+
+  virtual void StartPlay() override;
 };
